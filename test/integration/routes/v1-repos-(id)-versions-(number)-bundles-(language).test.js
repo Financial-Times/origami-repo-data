c98eb07b32708de4de48e91317caf8b00e44ff5b
Lee Moody <lee+github@leemoody.co.uk>
2019-07-11 22:01:28 +0100

    Test where no bundles. Refactor

--- test/integration/routes/v1-repos-(id)-versions-(number)-bundles-(language).test.js
+++ test/integration/routes/v1-repos-(id)-versions-(number)-bundles-(language).test.js
@@ -5,68 +5,59 @@ const assert = require('proclaim');
 const database = require('../helpers/database');
 const seed = require('../seed/basic/02-bundles');
 
-function assertBundle(expectedId, response) {
-    // Confirm the actual response has an object.
-    const actual = response.find(b => b.id === expectedId);
-    if(!actual) {
-        assert.ok(false, `Could not find bundle "${expectedId}" in the response.`);
-    }
-    assert.isObject(actual, 'Expected the bundle details to be an Object.');
-    assert.lengthEquals(Object.keys(actual), 7, 'Expected the return bundle details to have 8 keys.');
-    // Get the actual data for the bundle we expect.
-    const bundleData = seed.data.find(bundle => bundle.id === expectedId);
-    if (!bundleData) {
-        throw new Error(`Could not find actual ${expectedId} data to check the response.`);
-    }
-    // Assert the bundle we expected is represented in the response.
-    assert.strictEqual(actual.id, bundleData['id']);
-    assert.strictEqual(new Date(actual.created).toString(), bundleData['created_at'].toString());
-    assert.strictEqual(new Date(actual.updated).toString(), bundleData['updated_at'].toString());
-    assert.strictEqual(actual.versionId, bundleData['version_id']);
-    assert.strictEqual(actual.brand, bundleData['brand']);
-    assert.isObject(actual.sizes, 'Expected the bundle sizes to be an Object.');
-    assert.lengthEquals(Object.keys(actual.sizes), 2, 'Expected to have two bundle sizes.');
-    assert.strictEqual(actual.sizes.raw, bundleData['sizes']['raw']);
-    assert.strictEqual(actual.sizes.gzip, bundleData['sizes']['gzip']);
-    assert.strictEqual(actual.url, bundleData['url']);
-}
-
-
 describe('GET /v1/repos/:repoId/versions/:versionNumber/bundles/:language', () => {
 
     beforeEach(async () => {
         await database.seed(app, 'basic');
     });
 
-    const tests = [
-        {
+    const versions = {
+        noBundles: {
+            version: 'dbd71199-c1ab-4482-9988-eee350b3bdca',
+            isBranded: true,
+            hasBundles: false,
+        },
+        branded: {
             version: '9e4e450d-3b70-4672-b459-f297d434add6',
-            versionIsBranded: true,
-            brands: [],
+            isBranded: true,
+            hasBundles: true,
+        },
+        notBranded: {
+            version: 'b2bdfae1-cc6f-4433-9a2f-8a4b762cda71',
+            isBranded: false,
+            hasBundles: true,
+        }
+    };
+
+    const tests = [
+        Object.assign({}, versions.noBundles, {
+            language: 'js',
+            expectedBundles: [],
+            expectedDescription: 'An empty array.',
+        }),
+        Object.assign({}, versions.branded, {
             language: 'js',
             expectedBundles: ['3aa9eb66-058b-44aa-8b09-764c9801ae31'],
             expectedDescription: 'All JS bundles.',
-        },
-        {
-            version: '9e4e450d-3b70-4672-b459-f297d434add6',
-            versionIsBranded: true,
-            brands: ['none'],
+        }),
+        Object.assign({}, versions.branded, {
+            brand: 'none',
             language: 'js',
             expectedBundles: ['3aa9eb66-058b-44aa-8b09-764c9801ae31'],
             expectedDescription: 'All JS bundles',
-        },
-        {
-            version: '9e4e450d-3b70-4672-b459-f297d434add6',
-            versionIsBranded: true,
-            brands: ['master'],
+        }),
+        Object.assign({}, versions.branded, {
+            brand: 'master',
             language: 'js',
             expectedBundles: [],
             expectedDescription: 'No JS bundles (no JS bundles are branded at time of writing).',
-        },
-        {
-            version: '9e4e450d-3b70-4672-b459-f297d434add6',
-            versionIsBranded: true,
-            brands: [],
+        }),
+        Object.assign({}, versions.noBundles, {
+            language: 'css',
+            expectedBundles: [],
+            expectedDescription: 'An empty array.',
+        }),
+        Object.assign({}, versions.branded, {
             language: 'css',
             expectedBundles: [
                 '50a42415-df48-4643-bd9a-c05a57bcd544',
@@ -74,11 +65,9 @@ describe('GET /v1/repos/:repoId/versions/:versionNumber/bundles/:language', () =
                 '06d1fc27-e465-4e8f-8cb3-5a240f86ce55',
             ],
             expectedDescription: 'All CSS bundles.',
-        },
-        {
-            version: '9e4e450d-3b70-4672-b459-f297d434add6',
-            versionIsBranded: true,
-            brands: ['all'],
+        }),
+        Object.assign({}, versions.branded, {
+            brand: 'all',
             language: 'css',
             expectedBundles: [
                 '50a42415-df48-4643-bd9a-c05a57bcd544',
@@ -86,107 +75,93 @@ describe('GET /v1/repos/:repoId/versions/:versionNumber/bundles/:language', () =
                 '06d1fc27-e465-4e8f-8cb3-5a240f86ce55',
             ],
             expectedDescription: 'All CSS bundles.',
-        },
-        {
-            version: '9e4e450d-3b70-4672-b459-f297d434add6',
-            versionIsBranded: true,
-            brands: ['master'],
+        }),
+        Object.assign({}, versions.branded, {
+            brand: 'master',
             language: 'css',
             expectedBundles: [
                 '50a42415-df48-4643-bd9a-c05a57bcd544'
             ],
             expectedDescription: 'Only the master brand CSS bundle.',
-        },
-        {
-            version: '9e4e450d-3b70-4672-b459-f297d434add6',
-            versionIsBranded: true,
-            brands: ['master', 'internal'],
+        }),
+        Object.assign({}, versions.branded, {
+            brand: 'master,internal',
             language: 'css',
             expectedBundles: [
                 '50a42415-df48-4643-bd9a-c05a57bcd544',
                 '083f15a1-509b-44b2-88ab-7369c6e76326',
             ],
             expectedDescription: 'Only the CSS bundles for the explicitly requested brands.',
-        },
-        {
-            version: '9e4e450d-3b70-4672-b459-f297d434add6',
-            versionIsBranded: true,
-            brands: ['none'],
+        }),
+        Object.assign({}, versions.branded, {
+            brand: 'none',
             language: 'css',
             expectedBundles: [],
             expectedDescription: 'No CSS bundles.',
-        },
-        {
-            version: '9e4e450d-3b70-4672-b459-f297d434add6',
-            versionIsBranded: true,
-            brands: ['null'],
+        }),
+        Object.assign({}, versions.branded, {
+            brand: 'null',
             language: 'css',
             expectedBundles: [],
             expectedDescription: 'No CSS bundles.',
-        },
-        {
-            version: '9e4e450d-3b70-4672-b459-f297d434add6',
-            versionIsBranded: true,
-            brands: ['undefined'],
+        }),
+        Object.assign({}, versions.branded, {
+            brand: 'undefined',
             language: 'css',
             expectedBundles: [],
             expectedDescription: 'No CSS bundles.',
-        },
-        {
-            version: 'b2bdfae1-cc6f-4433-9a2f-8a4b762cda71',
-            versionIsBranded: false,
-            brands: [],
+        }),
+        Object.assign({}, versions.notBranded, {
             language: 'css',
             expectedBundles: ['05824698-d1d6-49f6-9c45-9216a5b45533'],
-            expectedDescription: 'Its CSS bundle.',
-        },
-        {
-            version: 'b2bdfae1-cc6f-4433-9a2f-8a4b762cda71',
-            versionIsBranded: false,
-            brands: ['none'],
+            expectedDescription: 'Its only CSS bundle.',
+        }),
+        Object.assign({}, versions.notBranded, {
+            brand: 'none',
             language: 'css',
             expectedBundles: ['05824698-d1d6-49f6-9c45-9216a5b45533'],
-            expectedDescription: 'Its CSS bundle.',
-        },
-        {
-            version: 'b2bdfae1-cc6f-4433-9a2f-8a4b762cda71',
-            versionIsBranded: false,
-            brands: ['all'],
+            expectedDescription: 'Its only CSS bundle.',
+        }),
+        Object.assign({}, versions.notBranded, {
+            brand: 'all',
             language: 'css',
             expectedBundles: [],
             expectedDescription: 'No CSS bundle.',
-        },
-        {
-            version: 'b2bdfae1-cc6f-4433-9a2f-8a4b762cda71',
-            versionIsBranded: false,
-            brands: ['null'],
+        }),
+        Object.assign({}, versions.notBranded, {
+            brand: 'null',
             language: 'css',
             expectedBundles: ['05824698-d1d6-49f6-9c45-9216a5b45533'],
-            expectedDescription: 'Its CSS bundle.',
-        },
-        {
-            version: 'b2bdfae1-cc6f-4433-9a2f-8a4b762cda71',
-            versionIsBranded: false,
-            brands: ['undefined'],
+            expectedDescription: 'Its only CSS bundle.',
+        }),
+        Object.assign({}, versions.notBranded, {
+            brand: 'undefined',
             language: 'css',
             expectedBundles: ['05824698-d1d6-49f6-9c45-9216a5b45533'],
-            expectedDescription: 'Its CSS bundle.',
-        },
-        {
-            version: 'b2bdfae1-cc6f-4433-9a2f-8a4b762cda71',
-            versionIsBranded: false,
-            brands: ['master'],
+            expectedDescription: 'Its only CSS bundle.',
+        }),
+        Object.assign({}, versions.notBranded, {
+            brand: 'master',
             language: 'css',
             expectedBundles: [],
             expectedDescription: 'No CSS bundle.',
-        },
+        }),
     ];
 
     tests.forEach(data => {
-        const { version, brands, language, expectedBundles, versionIsBranded, expectedDescription} = data;
-        const url = `/v1/repos/c990cb4b-c82b-5071-afb0-16149debc53d/versions/${version}/bundles/${language}${brands.length > 0 ? `?brand=${brands.join(',')}` : ''}`;
+        const {
+            version,
+            brand,
+            language,
+            expectedBundles,
+            isBranded,
+            hasBundles,
+            expectedDescription
+        } = data;
 
-        describe(`A request for ${language.toUpperCase()} bundles of a ${versionIsBranded ? 'branded' : 'non-branded'} version${brands.length > 0 ? `, with the brand parameter set to "${brands}"` : ''}.`, () => {
+        const url = `/v1/repos/c990cb4b-c82b-5071-afb0-16149debc53d/versions/${version}/bundles/${language}${brand ? `?brand=${brand}` : ''}`;
+
+        describe(`A request for ${language.toUpperCase()} bundles of a ${isBranded ? 'branded' : 'non-branded'} version${!hasBundles ? ', which has no bundle stats yet' : ''}${brand ? `, with the brand parameter set to "${brand}"` : ''}.`, () => {
             let request;
 
             before(async () => {
@@ -207,10 +182,35 @@ describe('GET /v1/repos/:repoId/versions/:versionNumber/bundles/:language', () =
 
             it(`JSON response has expected bundles: ${expectedDescription}`, async () => {
                 const response = (await request.then()).body;
+                // Confirm the response is an array of an expected size.
                 assert.isArray(response);
                 assert.lengthEquals(response, expectedBundles.length);
+                // Confirm that each item of the array is an expected bundle.
                 expectedBundles.forEach(expectedId => {
-                    assertBundle(expectedId, response);
+                    // Confirm a bundle with the correct id was given.
+                    const actual = response.find(b => b.id === expectedId);
+                    if (!actual) {
+                        assert.ok(false, `Could not find bundle "${expectedId}" in the response.`);
+                    }
+                    // Confirm the bundle has the expected properties.
+                    const expectedKeys = ['id','created','updated','versionId','brand','sizes','url'];
+                    assert.isObject(actual, 'Expected the bundle details to be an Object.');
+                    assert.deepEqual(Object.keys(actual).sort(), expectedKeys.sort(), 'Unexpected bundle properties where given in the response.');
+                    // Confirm all other bundle details against the seed data.
+                    const bundleData = seed.data.find(bundle => bundle.id === expectedId);
+                    if (!bundleData) {
+                        throw new Error(`Could not find actual ${expectedId} data to check the response.`);
+                    }
+                    assert.strictEqual(actual.id, bundleData['id']);
+                    assert.strictEqual(new Date(actual.created).toString(), bundleData['created_at'].toString());
+                    assert.strictEqual(new Date(actual.updated).toString(), bundleData['updated_at'].toString());
+                    assert.strictEqual(actual.versionId, bundleData['version_id']);
+                    assert.strictEqual(actual.brand, bundleData['brand']);
+                    assert.isObject(actual.sizes, 'Expected the bundle sizes to be an Object.');
+                    assert.lengthEquals(Object.keys(actual.sizes), 2, 'Expected to have two bundle sizes.');
+                    assert.strictEqual(actual.sizes.raw, bundleData['sizes']['raw']);
+                    assert.strictEqual(actual.sizes.gzip, bundleData['sizes']['gzip']);
+                    assert.strictEqual(actual.url, bundleData['url']);
                 });
             });
         });
