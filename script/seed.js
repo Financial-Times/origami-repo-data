9b8dad2f516e7c0dfbeeeacc787f395ce8544b27
Jake Champion <me@jakechampion.name>
2020-11-27 17:02:32 +0000

    Disable checking certificate on SSL connections based on Heroku guidance
    https://help.heroku.com/MDM23G46/why-am-i-getting-an-error-when-i-upgrade-to-pg-8

--- script/seed.js
+++ script/seed.js
@@ -3,14 +3,19 @@
 
 const dotenv = require('dotenv');
 const knex = require('knex');
+const parse = require('pg-connection-string').parse;
 
 // Load options from an .env file if present
 dotenv.config();
 
+const connectionConfig = parse(process.env.DATABASE_URL || 'postgres://localhost:5432/origami-repo-data');
+if (connectionConfig.host !== 'localhost') {
+	connectionConfig.ssl = { rejectUnauthorized: false };
+}
 // Connect to the database
 const database = knex({
 	client: 'pg',
-	connection: process.env.DATABASE_URL || 'postgres://localhost:5432/origami-repo-data'
+	connection: connectionConfig
 });
 
 // Seed the database
