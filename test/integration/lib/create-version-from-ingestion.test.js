7461cf0369ca488d1c12bd07f4f214c329710ad1
notlee <notlee@users.noreply.github.com>
2021-03-12 11:12:39 +0000

    Fix Sass language check for v2 component ingestions
    Sass was always set as a language since the promise was not awaited.
    It would have eventually thrown an error as the returned Sass file
    would be parsed as json.

--- test/integration/lib/create-version-from-ingestion.test.js
+++ test/integration/lib/create-version-from-ingestion.test.js
@@ -30,20 +30,39 @@ describe('createVersionFromIngestion', () => {
     });
 
     describe('given a v2 component Ingestion', () => {
-        it('saves a new Version if the spec v2 component Ingestion is valid', async () => {
-            const url = 'https://github.com/Financial-Times/o-test-component';
-            const tag = 'v2.0.1';
+        describe('that is valid, with JS and Sass', () => {
+            it('saves a new Version with languages set', async () => {
+                const url = 'https://github.com/Financial-Times/o-test-component';
+                const tag = 'v2.0.1';
 
-            const Ingestion = app.model.Ingestion;
-            const ingestion = await new Ingestion({ url, tag, type: 'version' });
+                const Ingestion = app.model.Ingestion;
+                const ingestion = await new Ingestion({ url, tag, type: 'version' });
 
-            await createVersionFromIngestion(ingestion, app.model.Version, app.github);
+                await createVersionFromIngestion(ingestion, app.model.Version, app.github);
 
-            const version = await app.model.Version.fetchOneByUrlAndTag(url, tag);
+                const version = await app.model.Version.fetchOneByUrlAndTag(url, tag);
 
-            proclaim.ok(version, 'No Version could be found for the Ingestion.');
-            proclaim.include(version.get('languages'), 'js', 'Expected the Version to include "js" in the "languages" property.');
-            proclaim.include(version.get('languages'), 'scss', 'Expected the Version to include "scss" in the "languages" property.');
+                proclaim.ok(version, 'No Version could be found for the Ingestion.');
+                proclaim.include(version.get('languages'), 'js', 'Expected the Version to include "js" in the "languages" property.');
+                proclaim.include(version.get('languages'), 'scss', 'Expected the Version to include "scss" in the "languages" property.');
+            });
+        });
+        describe('that is valid, with JS but no Sass', () => {
+            it('saves a new Version with languages set', async () => {
+                const url = 'https://github.com/Financial-Times/o-test-component';
+                const tag = 'v2.0.16';
+
+                const Ingestion = app.model.Ingestion;
+                const ingestion = await new Ingestion({ url, tag, type: 'version' });
+
+                await createVersionFromIngestion(ingestion, app.model.Version, app.github);
+
+                const version = await app.model.Version.fetchOneByUrlAndTag(url, tag);
+
+                proclaim.ok(version, 'No Version could be found for the Ingestion.');
+                proclaim.include(version.get('languages'), 'js', 'Expected the Version to include "js" in the "languages" property.');
+                proclaim.notInclude(version.get('languages'), 'scss', 'Expected the Version to not include "scss" in the "languages" property, "js" only.');
+            });
         });
     });
 });
